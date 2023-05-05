dfca65bbf631fb3cbd297b8c5daaf289d3455fa3
Mambulya <68922098+Mambulya@users.noreply.github.com>
2023-05-05 12:54:47 +0300

    Create Probability&FrequencyHistorgam.py
    The script draws firstly a frequency histogram, and secondly a probability histogram

---
+++ Probability&FrequencyHistorgam.py
@@ -0,0 +1,77 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import openpyxl
+
+
+# считываем данные
+path = "./data.xlsx"
+book = openpyxl.open(path, read_only=True)
+sheet = book.active
+data = []
+
+for row in range(1, 100000):
+    try:
+        data.append(float(sheet[row][0].value))
+    except IndexError:
+        break
+    except ValueError:
+        continue
+
+
+# находим характеристики распределения
+volume = len(data)
+MAX = max(data)
+MIN = min(data)
+diff = MAX - MIN
+Ex = sum(data) / volume                             # выборочное среднее - состоятельная и не смещенная оценка параметра
+Dx = np.var(data)                                   # дисперсия var() - смещенная
+б = Dx**(1/2)                                       # среднее квадротическое
+
+# Гисторграммы - частотная/вероятностная
+k = int(volume/5)                                   # количество интервалов
+step = (MAX - MIN) / (k-1)                          # шаг между интервалами
+interval1 = MIN + step/2
+intervalK = MAX - step/2                            # последняя метка
+intervals = [interval1]
+
+# формируем метки интеравалов
+for i in range(1, k-1):
+    intervals.append(intervals[i-1] + step)
+intervals.append(intervalK)
+
+
+# рисуем гистограмму распределения
+fig, diagram = plt.subplots(figsize =(13, 6))      # размер картинки
+
+plt.hist(data,intervals, color="pink", ec="red")
+plt.xticks(intervals)
+plt.xlabel("Values")
+plt.ylabel("Amount")
+plt.title("Statistics Histogram")
+plt.legend("Values", loc=2, frameon=False)
+plt.grid(linestyle='--', color='pink')
+plt.show()
+
+fig1, diagram1 = plt.subplots(figsize =(13, 6))
+plt.hist(data,intervals, color="pink", ec="red", density=True)
+plt.xticks(intervals)
+plt.xlabel("Values")
+plt.ylabel("Probability")
+plt.title("Probability Histogram")
+plt.legend("Values", loc=2, frameon=False)
+plt.grid(linestyle='--', color='pink')
+plt.show()
+
+# OUTPUT
+print(
+"""
+MAX = {}
+MIN = {}
+количество интервалов (k) = {}
+step ~ {}
+interval 1 ~ {}
+interval K ~ {}
+Ex ~ {}
+б ~ {}
+Dx = б^2 ~ {}
+""".format( MAX, MIN, k, np.round(step, 3), np.round(interval1, 3), np.round(intervalK, 3), np.round(Ex, 3), np.round(б, 3), np.round(Dx, 3)))
