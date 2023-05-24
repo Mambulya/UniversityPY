216250abb81e50d5b3952a162239a0dbc0e37ac9
Mambulya <68922098+Mambulya@users.noreply.github.com>
2023-05-24 15:02:48 +0300

    main.py
    Главный запкскаемый файл
    (Регрессия осуществляется Y по X)

--- MatStaT/laboratory2/main.py
+++ MatStaT/laboratory2/main.py
@@ -1,10 +1,13 @@
 from SampleClass import Sample
 
-Sample1 = Sample("./dataYX.xlsx")
+Sample1 = Sample("./16variant.xlsx")
 
-Sample1.print_momnets()
+Sample1.print_momenets()
 Sample1.print_correlation()
 Sample1.print_equation()
 Sample1.print_det()
-Sample1.predict(x=80, p=95)
+Sample1.print_factf(p=95)
+print("a: {}, b: {}".format(Sample1.a, Sample1.b))
+Sample1.predictf(x=116, p=95)
+
 Sample1.draw_dot_graph()
