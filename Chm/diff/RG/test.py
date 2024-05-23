a97e8eee07b71949a41b4a15ed566605b96d7efe
Mambulya <68922098+Mambulya@users.noreply.github.com>
2024-05-23 13:44:02 +0300

    рунге-кутты
    тестовая система уравнений + графики

--- Chm/diff/RG/test.py
+++ Chm/diff/RG/test.py
@@ -9,6 +9,7 @@ y1(0) = 0, y2(0) = 0
 [0, 4]
 """
 from math import e
+import matplotlib.pyplot as plt
 
 def f1Test(t, x1, x2):
     return -2 * x1 - 3*x2 + 4*e**(-t)
@@ -23,7 +24,6 @@ def RGtest(h = 0.5):
     y0 = 0
     y1 = y0     # yi и y(i-1)
     y2 = y0
-    error = -1
 
     for i in range(int((b-a) / h)):
         # вычисляем y1 на шаге i - при этом берем y2 при i
@@ -40,17 +40,42 @@ def RGtest(h = 0.5):
         k4 = f1Test(t + h, y1New + h*k1 - 2*h*k2 + 2*h*k3, y2)
         y2 += (h/6) * (k1 + 4*k3 + k4)
 
+        E = e ** (-t)
+
         # обновляем данные
         y1 = y1New
-
-        E = e**(-t)
-
         t += h
-        
-    print(max(abs(y1 - E), abs(y2 - E)))    # максимальная погрешность на последнем шаге
+
+    return max(abs(y1 - E), abs(y2 - E))    # максимальная погрешность на последнем шаге
 
         # проверка
         # if 1.0 <= t <= 1.1:
         #     print("i = {}: y1 = {}   y2 = {}".format(i, y1, y2))
 
-RGtest(h = 0.03)
+hs = [0.01, 0.03, 0.07, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
+errors = []
+
+for h in hs:
+    errors.append(RGtest(h))
+
+plt.plot(hs, errors, label = "е = e(h)", color = "#de2ad8")
+
+plt.title("Погрешность e при h")
+plt.ylabel("e")
+plt.xlabel("h")
+plt.legend()
+plt.grid()
+plt.show()
+
+
+# зависимость e / h**4
+new_errors = [errors[i] / (hs[i]**4) for i in range(len(hs))]
+plt.plot(hs, new_errors, label = "e/h^4 от h", color = "#0fbfff")
+plt.ylim((0, 60))
+
+plt.title("Погрешность e/h^4 при h")
+plt.ylabel("e/h^4")
+plt.xlabel("h")
+plt.legend()
+plt.grid()
+plt.show()
